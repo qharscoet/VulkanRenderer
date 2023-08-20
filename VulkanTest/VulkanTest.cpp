a45d17a9579818b549ab0bb9b72bb3f31e504ef6
Quentin Harscoët <harscoet.quentin@gmail.com>
2023-08-20 23:50:25 +0200

    Instance creation

--- VulkanTest/VulkanTest.cpp
+++ VulkanTest/VulkanTest.cpp
@@ -4,55 +4,124 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class HelloTriangleApplication {
 public:
-    void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
 
 private:
-    GLFWwindow* window;
+	GLFWwindow* window;
+	VkInstance instance;
 
 private:
-    void initWindow() {
-        glfwInit();
+	void initWindow() {
+		glfwInit();
 
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-        window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
 
-    }
+	}
 
-    void initVulkan() {
+	bool checkExtensions(uint32_t& glfwExtensionCount, const char**& glfwExtensions) {
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
 
-    }
 
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-    }
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-    void cleanup() {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
+		std::cout << "GLFW requires " << glfwExtensionCount << " extensions" << std::endl;
+		bool all_found = true;
+		for (int i = 0; i < glfwExtensionCount; i++) {
+			std::cout << "Checking presence of " << glfwExtensions[i];
+
+			bool found = false;
+			for (const auto& extension : extensions) {
+				if (strcmp(extension.extensionName, glfwExtensions[i]) == 0)
+				{
+					found = true;
+					break;
+				}
+			}
+			if (found)
+			{
+				std::cout << '\t\t' <<  "FOUND"<< '\n';
+			}
+			else {
+				std::cout << '\t\t' << "NOT FOUND" << '\n';
+				all_found = false;
+			}
+			
+		}
+
+		return all_found;
+	}
+
+	void createInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+		appInfo.pEngineName = "No Engine Yet";
+		appInfo.engineVersion = VK_MAKE_API_VERSION(0, 0, 0, 1);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		if (!checkExtensions(glfwExtensionCount, glfwExtensions)) {
+			throw std::runtime_error("failed to create instance : unsupported extensions");
+		}
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
+
+	void initVulkan() {
+		createInstance();
+	}
+
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
+
+	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
 };
 
 int main() {
-    HelloTriangleApplication app;
+	HelloTriangleApplication app;
 
-    try {
-        app.run();
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
